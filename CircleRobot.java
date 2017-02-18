f404424a3073e1f8832ff5aa190f0eaf9345589c
Daniel Koberstein <daniel.koberstein@outlook.com>
2017-02-18 11:43:01 -0800

    Update

--- CircleRobot.java
+++ CircleRobot.java
@@ -29,26 +29,19 @@ public class CircleRobot extends AdvancedRobot
 		// Robot main loop
 		while(true) {
 			turnRadarLeft(10000);
-			PointAt(targetRobot);
-			double gunHeading = getGunHeading();
-			RobotData targetRobot = this.robotMap.get(this.targetRobot);
-			// Replace the next 4 lines with any behavior you would like
-			// ahead(100);
-			// turnGunRight(360);
-			// back(100);
-			// turnGunRight(360);
+			this.PointAt(targetRobot);
 		}
 	}
 
-	public void PointAt(string target)
+	public void PointAt(String target)
 	{
 		double x = getX();
 		double y = getY();
 		RobotData robot = this.robotMap.get(this.targetRobot);
 		double targetGunHeading = Math.atan((robot.Y-y)/(robot.X-x));
 		double currentGunHeading = getGunHeading();
-		
-		
+		double turnAngle = currentGunHeading - targetGunHeading;
+		setTurnGunLeft(turnAngle);
 	}
 
 	/**
@@ -56,8 +49,15 @@ public class CircleRobot extends AdvancedRobot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Replace the next line with any behavior you would like
-		RobotData robotData = new RobotData();
-		//robotMap.put(e.getName(), e);
+		double distance = e.getDistance();
+		double bearing = e.getBearing();
+
+		RobotData robot = new RobotData();
+		robot.X = 10; // TODO: calculate x and y
+		robot.Y = 10;
+		robot.Energy = e.getEnergy();
+		robot.Heading = e.getHeading();
+		robotMap.put(e.getName(), robot);
 		if (targetRobot == "")
 		{
 			targetRobot = e.getName();
